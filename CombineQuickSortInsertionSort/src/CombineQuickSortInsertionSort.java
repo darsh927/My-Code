78cb05bf4c2ce306c6c3c52cf1f32b199ca484e5
darsh927 <darsh927@yahoo.com>
2015-12-09 12:12:27 -0500

    Update CombineQuickSortInsertionSort.java

--- CombineQuickSortInsertionSort/src/CombineQuickSortInsertionSort.java
+++ CombineQuickSortInsertionSort/src/CombineQuickSortInsertionSort.java
@@ -1,54 +1,48 @@
 /**
  * Created by Darsh on 11/18/15.
  */
-
-public class CombineQuickSortInsertionSort {
+public class QuickSortIntoInsertionSort {
     private long[] data;
-
-    private int len;
+    private int lenght;
     int count=0;
     int iCount=0;
-
-    public CombineQuickSortInsertionSort(int max) {
+    
+    public static void main(String[] args) {
+    	QuickSortIntoInsertionSort array = new QuickSortIntoInsertionSort(10000);
+    	
+    	for (int j = 0; j < 10000; j++){
+    		long n = (int) (java.lang.Math.random() * 100);
+    		array.insert(n);
+            }
+            array.display();
+            array.quickSort();
+            array.display();
+            }
+    public QuickSortIntoInsertionSort(int max) {
         data = new long[max];
-        len = 0;
+        lenght = 0;
     }
-
     public void insert(long value) {
-        data[len] = value;
-        len++;
+        data[lenght] = value;
+        lenght++;
     }
-
-    public void display() {
-        System.out.print("Data:");
-        for (int j = 0; j < len; j++)
-            System.out.print(data[j] + " ");
-        System.out.println("");
-        System.out.println("QuickSort Run: "+count);
-        System.out.println("Insertion Sort Run: "+iCount);
-
-    }
-
-
     public void quickSort() {
-        recQuickSort(0, len - 1);
+        recQS(0, lenght - 1);
     }
-
-    public void recQuickSort(int left, int right) {
+    public void recQS(int left, int right) {
         int size = right - left + 1;
-        //THIS IS WHERE THE ARRAY IS SWITCHED TO INSERTION SORT
-        if (size < 50) // insertion sort if small
+        //>>>>>>>>>>>>>>>>>>>>>>>>>>THIS IS WHERE THE ARRAY IS SWITCHED TO INSERTION SORT <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        if (size < 10) // insertion sort if small
             insertionSort(left, right);
-        else // quicksort if large
+        else // Quick sort if large
         {
-            long median = medianOf3(left, right);
+            long median = midPoint(left, right);
             int partition = partitionIt(left, right, median);
-            recQuickSort(left, partition - 1);
-            recQuickSort(partition + 1, right);
+            recQS(left, partition - 1);
+            recQS(partition + 1, right);
+            }
         }
-    }
-
-    public long medianOf3(int left, int right) {
+    public long midPoint(int left, int right) {
         int center = (left + right) / 2;
         // order left & center
         if (data[left] > data[center])
@@ -56,68 +50,58 @@ public class CombineQuickSortInsertionSort {
         // order left & right
         if (data[left] > data[right])
             swap(left, right);
-        // order center & right
+        //order center & right
         if (data[center] > data[right])
             swap(center, right);
-
+        
         swap(center, right - 1);
         return data[right - 1];
     }
-
     public void swap(int d1, int d2) {
         long temp = data[d1];
         data[d1] = data[d2];
         data[d2] = temp;
     }
-
     public int partitionIt(int left, int right, long pivot) {
-        int leftPtr = left; // right of first elem
+        int leftPtr = left; // right of first node
         int rightPtr = right - 1; // left of pivot
+       
         while (true) {
             //find bigger
-            while (data[++leftPtr] < pivot)
-                ;
+            while (data[++leftPtr] < pivot);
             //find smaller
-            while (data[--rightPtr] > pivot)
-                ;
-            if (leftPtr >= rightPtr) // if pointers cross, partition done
+            while (data[--rightPtr] > pivot);
+            
+            if (leftPtr >= rightPtr) //if pointers cross, partition finished
                 break;
             else
                 swap(leftPtr, rightPtr);
         }
-        swap(leftPtr, right - 1); // restore pivot
+        swap(leftPtr, right - 1); //restore pivot
         count++;
-        return leftPtr; // return pivot location
+        return leftPtr; //return pivot location
     }
-
     public void insertionSort(int left, int right) {
         int in, out;
         //  sorted on left of out
         for (out = left + 1; out <= right; out++) {
-            long temp = data[out]; // remove marked item
-            in = out; // start shifts at out
-            // until one is smaller,
+            long temp = data[out]; // remove marked node
+            in = out; // start shifts at out until one is smaller
             while (in > left && data[in - 1] >= temp) {
-                data[in] = data[in - 1]; // shift item to right
-                --in; // go left one position
+                data[in] = data[in - 1]; //shift node to right
+                --in; //go left one position
             }
             data[in] = temp; // insert marked item
             iCount++;
+            }
+        }
+    //Method to display Array Data created and count variables for both Insertion sort and QuickSort
+    public void display() {
+        System.out.print("Data:");
+        for (int j = 0; j < lenght; j++)
+            System.out.print(data[j] + " ");
+        System.out.println("");
+        System.out.println("Quick Sort count: "+count);
+        System.out.println("Insertion Sort count: "+iCount);
         }
     }
-
-    public static void main(String[] args) {
-        //SIZE OF THE ARRAY
-        int maxSize = 10000;
-        CombineQuickSortInsertionSort arr = new CombineQuickSortInsertionSort(maxSize);
-
-        for (int j = 0; j < maxSize; j++)
-        {
-            long n = (int) (java.lang.Math.random() * 100000);
-            arr.insert(n);
-        }
-            arr.display();
-            arr.quickSort();
-            arr.display();
-    }
-}
